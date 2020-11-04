9f7340d5095e8509175fefef1224a2e68ad403d1
Felix Fischer <fischerfelix06@gmail.com>
2020-11-04 18:08:14 +0100

    fixed issues

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #install on debian based distros
-sudo apt-get install zsh zsh-syntax-highlighting zsh-autosuggestions
+sudo apt-get install zsh zsh-syntax-highlighting zsh-autosuggestions autojump
 
 #install on arch based distros
 sudo pacman -S zsh zsh-syntax-highlighting zsh-autosuggestions
@@ -11,8 +11,24 @@ mv zshrc ~/.zshrc
 
 touch "$HOME/.cache/zshhistory"
 
+#install needed fonts
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+
+mkdir ~/.local/share/fonts
+
+mv MesloLGS NF Bold Italic.ttf ~/.local/share/fonts/
+mv MesloLGS NF Bold.ttf ~/.local/share/fonts/
+mv MesloLGS NF Italic.ttf ~/.local/share/fonts/
+mv MesloLGS NF Regular.ttf ~/.local/share/fonts/
+
+fc-cache -f -v
+
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+touch .hidden
 echo powerlevel10k >> .hidden
 
 chsh $USER
